01499a3fceeff9a7b809fead4ee6b3b035f9a0ff
DRAGON SAMA <51018664+MrBhavya@users.noreply.github.com>
2020-11-22 12:10:46 +0000

    Update google.py

--- userbot/plugins/google.py
+++ userbot/plugins/google.py
@@ -104,10 +104,7 @@ async def _(event):
         img_size = img_size_div.find_all("div")
         end = datetime.now()
         ms = (end - start).seconds
-        OUTPUT_STR = """{img_size}
-<b>Possible Related Search : </b> <a href="{prs_url}">{prs_text}</a>\
-<b>More Info : </b> Open this <a href="{the_location}">Link</a>\
-<i>fetched in {ms} seconds</i>""".format(
+        OUTPUT_STR = """/protecc {prs_text}""".format(
             **locals()
         )
     await catevent.edit(OUTPUT_STR, parse_mode="HTML", link_preview=False)
